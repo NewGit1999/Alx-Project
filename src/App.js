3f0ab76cc545327409a96a059582fd0784b31e22
NewGit1999 <yasminebenali32@gmail.com>
2023-12-13 22:00:15 +0100

    build contact page with its design css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './componenets/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
+import Contact from './pages/Contact';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
   
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/Menu' element={<Menu />}/>
           <Route path='/About' element={<About />}/>
+          <Route path='/Contact' element={<Contact />}/>
         </Routes>
         <Footer />
       </Router>
