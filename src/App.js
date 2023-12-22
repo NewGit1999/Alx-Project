e331270cf9f20d87ba94ae903b036e54113851aa
NewGit1999 <yasminebenali32@gmail.com>
2023-12-22 18:51:23 +0100

    build completed login and register pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/Register';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
   
@@ -17,10 +19,14 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Login />} />
+          <Route path='/Home' element={<Home />}/>
           <Route path='/Menu' element={<Menu />}/>
           <Route path='/About' element={<About />}/>
           <Route path='/Contact' element={<Contact />}/>
+          
+          <Route path='/sign-up' element={<SignUp />}/>
+
         </Routes>
         <Footer />
       </Router>
